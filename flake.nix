007abf6c387d41b83d01a4e8ebfbb4f52749b995
Dessera <1533653159@qq.com>
2024-09-18 08:26:17 +0800

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,39 @@
 {
-  # This example flake.nix is pretty generic and the same for all
-  # examples, except when they define devShells or extra packages.
-  description = "Dream2nix example flake";
-
-  # We import the latest commit of dream2nix main branch and instruct nix to
-  # re-use the nixpkgs revision referenced by dream2nix.
-  # This is what we test in CI with, but you can generally refer to any
-  # recent nixpkgs commit here.
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.follows = "dream2nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-  }: let
-    # A helper that helps us define the attributes below for
-    # all systems we care about.
-    eachSystem = nixpkgs.lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-  in {
-    packages = eachSystem (system: {
-      # For each system, we define our default package
-      # by passing in our desired nixpkgs revision plus
-      # any dream2nix modules needed by it.
-      default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./default.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
-      };
-    });
-  };
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rustTools = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain (_: rustTools);
+      in
+      rec {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+
+          # Add extra inputs here or any other derivation settings
+          # doCheck = true;
+          # buildInputs = [];
+          # nativeBuildInputs = [];
+        };
+
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [ nil nixpkgs-fmt ];
+        };
+      });
 }
