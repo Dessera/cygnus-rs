fbaa16eb621bfec2d48e4a7a67db29fe613a985d
Dessera <1533653159@qq.com>
2024-10-11 20:29:59 +0800

    更新了nixos模块

--- flake.nix
+++ flake.nix
@@ -32,6 +32,42 @@
           # nativeBuildInputs = [];
         };
 
+        nixosModules = {
+          default = { config, lib, ... }:
+            let
+              cfg = config.modules.services.cygnus;
+              inherit (lib) mkOption mkEnableOption mkIf types;
+            in
+            {
+              options.modules.services.cygnus = {
+                enable = mkEnableOption "Enable JLU Network Auth Service";
+                userFile = mkOption {
+                  type = types.str;
+                  description = "Path to the user file";
+                };
+              };
+
+              config = mkIf cfg.enable {
+                environment.systemPackages = [
+                  self.packages.default
+                ];
+
+                systemd.services.cygnus-auth = {
+                  description = "JLU Network Auth Service";
+                  enable = true;
+                  after = [ "network.target" ];
+                  wantedBy = [ "multi-user.target" ];
+                  serviceConfig = {
+                    Type = "simple";
+                    ExecStart = "${self.packages.default}/bin/cygnus -f ${cfg.userFile}";
+                    Restart = "always";
+                    RestartSec = 5;
+                  };
+                };
+              };
+            };
+        };
+
         devShells.default = craneLib.devShell {
           packages = with pkgs; [ nil nixpkgs-fmt ];
         };
